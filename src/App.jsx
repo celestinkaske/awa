f43f277d7ef3ed458b9bc8aeb0c4f46039610188
celeska <76659869+celeska@users.noreply.github.com>
2024-04-26 19:02:22 +0200

    chore: init tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className='bg-gray-900 text-white min-h-screen'>
+      <div className='pt-6'>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} alt="React logo" />
         </a>
       </div>
-      <h1>AWA</h1>
-      <div className="card">
+      <h1 className='font-bold text-xl sm:text-3xl lg:text-5xl'>AWA</h1>
+      <div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,10 +24,10 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p>
         This will become a daily helper!
       </p>
-    </>
+    </div>
   )
 }
 
