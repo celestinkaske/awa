b176c72da1ba335c9bfb0f81feb742d547042915
celeska <76659869+celeska@users.noreply.github.com>
2024-04-26 18:30:09 +0200

    chore: change name

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This will become a daily helper!
       </p>
     </>
   )
